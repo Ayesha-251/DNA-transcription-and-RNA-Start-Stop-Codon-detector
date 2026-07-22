1e18e48f1c70643ce768c32c0d7abefd1e0c21a1
Ayesha Ansar <ayeshaansar678@gmail.com>
2026-07-22 22:18:02 +0500

    Update DNA transcription and RNA start & stop codon detector.py

--- DNA transcription and RNA start & stop codon detector.py
+++ DNA transcription and RNA start & stop codon detector.py
@@ -1,23 +1,31 @@
 #DNA to RNA and codon determination:
 DNA= input("Enter a DNA sequence: ").upper().strip()
-RNA=DNA.replace("T", "U")
-print(f"The transcribed RNA sequence is {RNA}")
 RNA_start_codon=["AUG"]
 RNA_start_codon_found=False
 RNA_stop_codon=["UAG", "UGA", "UAA"]
 RNA_stop_codon_found=False
-if len(RNA)%3!=0:
-    print("WARNING!!!, not accurate RNA length for codon determination")
-else:
-    for i in range(0,len(RNA),3):
-        if RNA[i:i+3] in RNA_start_codon:
-            print(f"Start codon present at position {i}")
-            RNA_start_codon_found=True
-        if RNA[i:i+3] in RNA_stop_codon:
-            print(f"RNA stop codon present at position {i}")
-            RNA_stop_codon_found=True
-    if not RNA_start_codon_found:
-        print("No start codon found")
-    if not RNA_stop_codon_found:
-        print("No stop codon found")
+Invalid_Seq=False
+for i in range(len(DNA)):
+    if DNA[i] not in "ATGC":
+        print("Invalid DNA sequence")
+        Invalid_Seq=True
+        break
+if not Invalid_Seq:
+        RNA=DNA.replace("T", "U")
+        print(f"The transcribed RNA sequence is {RNA}")
+        if len(RNA)%3!=0:
+            print("WARNING!!!, not accurate RNA length for codon determination")
+        else:
+            for i in range(0,len(RNA),3):
+                if RNA[i:i+3] in RNA_start_codon:
+                    print(f"Start codon present at position {i}")
+                    RNA_start_codon_found=True
+                if RNA[i:i+3] in RNA_stop_codon:
+                    print(f"RNA stop codon present at position {i}")
+                    RNA_stop_codon_found=True
+            if not RNA_start_codon_found:
+                print("No start codon found")
+            if not RNA_stop_codon_found:
+                print("No stop codon found")
     
+
